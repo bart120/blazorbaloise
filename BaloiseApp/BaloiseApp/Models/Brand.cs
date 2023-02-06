0129cd225ac4556413d7d93c41f249108dd07872
Vincent Leclerc <vleclerc@inow.fr>
2023-02-06 14:34:08 +0100

    brands

---
+++ BaloiseApp/BaloiseApp/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace BaloiseApp.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+        public string?  Name { get; set; }
+        public string?  Image { get; set; }
+    }
+}
--- BaloiseApp/BaloiseApp/Program.cs
+++ BaloiseApp/BaloiseApp/Program.cs
@@ -1,11 +1,22 @@
 using BaloiseApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration.Memory;
+
+var confData = new Dictionary<string, string>()
+{
+    {"menuBgColor" , "red" },
+    {"urlApiAuth" , "https://..." },
+};
+
+var memoryConfig = new MemoryConfigurationSource { InitialData= confData };
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Configuration.Add(memoryConfig);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(@"https://formation.inow.fr/demo/api/v1/") });
 
 await builder.Build().RunAsync();
